93f3c4df3c79277bff818910d8a2b3c4f5db1247
Gorbataras <gorbataras@gmail.com>
2021-03-11 05:27:38 -0800

    First commit

---
+++ rules.mk
@@ -0,0 +1,5 @@
+MOUSEKEY_ENABLE     = yes     # Mouse keys
+RGBLIGHT_ENABLE     = no    # Enable WS2812 RGB underlight.
+VIA_ENABLE          = yes         # Enable VIA
+OLED_DRIVER_ENABLE  = no
+LTO_ENABLE          = no
